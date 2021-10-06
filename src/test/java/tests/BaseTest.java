bf7f29336fd5623acd7a6718ecd14795e97cb3a6
orlenkoev <orlenkoev@gmail.com>
2021-10-06 18:30:17 +0300

    last changes

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -39,7 +39,6 @@ public abstract class BaseTest {
             }
         }
         driver.get("https://demo.prestashop.com/");
-        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         BasePage.setThreadLocalDriver(driver);
 
